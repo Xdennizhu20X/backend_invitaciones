6abc2bde158a1c74b448db50c43c91bd05d88027
xennis Rodriguez <xennislrj25@gmail.com>
2024-07-12 21:21:56 -0500

    first commit

---
+++ conexions/mongo.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/boda_milton');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Error de conexión a la base de datos:'));
+db.once('open', () => {
+  console.log('¡Conexión a la base de datos establecida correctamente!');
+});
